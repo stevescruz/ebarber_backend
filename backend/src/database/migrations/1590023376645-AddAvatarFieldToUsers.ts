2d4e52b907fa15c80fe56f9f68370186b2d6ba1e
stevescruz <steve.silverio.cruz@gmail.com>
2020-05-24 12:11:15 -0400

    Uploading Files & Serving Static Files
    *users table
    
    -Migration for AddAvatarFieldToUsers CREATED
      -avatar field to users table ADDED
    -Migration for AddAvatarFieldToUsers EXECUTED
    
    -User.ts EDITED
      -Included avatar field to the model that represents the
    user entity.
    
    *Uploading Files (Config, Service and Routes)
    
    -tmp directory CREATED
    
    -Upload.ts CREATED
      -multer - dependency ADDED
      -@types/multer - developer dependency ADDED
      -Includes the path to the directory where the uploaded files
    will be saved.
      -Includes the rules for naming the uploaded files.
    
    -UpdateUserAvatarService.ts CREATED
      -Checks if the user is authenticated.
      -Checks if the user already has an avatar, If so Checks if the
    avatar file still exists, and if so delete the current avatar file.
      -Inserts the avatar filename into a user's avatar in the Database.
    
    -users.routes.ts EDITED
      -Included PATCH Method at the '/avatar' path.
        -Saves a file into the directory specified by upload.ts.
        -Saves a new avatar into an existing user in the database.
        -Deletes user's password property before returning a response with
    a JSON containing the user to avoid security vulnerabilities.
    
    *Serving Static Files
    
    server.ts EDITED
      -Mounts the express library's static middleware at the '/files' path
    on app express to serve static files from the directory contained in
    the upload.ts config file.
    
    *Other
    
    -.gitignore EDITED
      -Ignore all files inside the tmp directory.
      -NOT ignore the .gitkeep file inside the tmp directory.
    
    -.gitkeep CREATED
      -So git doesn't ignore the tmp folder.
    
    -package.json EDITED
      -"dev:server" script should now execute "ts-node-dev --inspect --transpileOnly
    --ignore-watch node_modules src/server.ts"

---
+++ backend/src/database/migrations/1590023376645-AddAvatarFieldToUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn, Column } from 'typeorm';
+
+export default class AddAvatarFieldToUsers1590023376645
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar');
+  }
+}
---
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface RequestDTO {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: RequestDTO): Promise<User> {
+    const usersRepository = getRepository(User);
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users may change their avatar');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+export default UpdateUserAvatarService;
