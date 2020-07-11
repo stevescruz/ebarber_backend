2ce3f05c4b14b31f61bfdc93dc9069f33116de84
stevescruz <steve.silverio.cruz@gmail.com>
2020-07-11 03:23:13 -0400

    CORS

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -11,6 +12,8 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
