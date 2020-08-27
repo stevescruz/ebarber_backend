cee182cb24587a1ee2467ba8e0118e5920ea1e09
stevescruz <steve.silverio.cruz@gmail.com>
2020-08-26 20:25:05 -0400

    Send Email Password Recovery Service Unit Test (Part III)
      -Created a unit test that checks if Send Email Password Recovery
        Service complies with:
    
        -The user shouldn't be able to recover his password by informing a non-existent email.
    
      -Created UserToken.ts
      -Created IUserTokensRepository.ts
      -Created UserTokensRepository.ts

--- src/modules/users/services/SendEmailPasswordRecoveryService.ts
+++ src/modules/users/services/SendEmailPasswordRecoveryService.ts
@@ -5,6 +5,8 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
 // import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
@@ -18,14 +20,18 @@ class SendEmailPasswordRecoveryService {
     private usersRepository: IUsersRepository,
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError('User does not exist.');
     }
+
+    await this.userTokensRepository.generateUserToken(user.id);
 
     this.mailProvider.sendEmail(email, 'Request for password reset received.');
   }
