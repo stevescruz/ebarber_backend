3676c214262cec4512d87cfd4312e9d27fb8d288
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-27 01:51:10 -0500

    Storing JWT in the Database (Part IV) -Created UserTokens Repository with TypeORM methods for findByToken and generateUserToken.

---
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,28 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepository.findOne(token);
+
+    return userToken;
+  }
+
+  public async generateUserToken(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({ user_id });
+    await this.ormRepository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepository;
