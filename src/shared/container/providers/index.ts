5a0c9f11ed1fccff7b6a0154746338dc1b1d813d
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-28 20:26:14 -0500

    FEAT: Emails for Development Environment (Part III) -Update provider container to register EtherealMailProvider in the provider IoC container for dependency injection.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
