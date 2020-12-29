a247c351bdecb2d0da64c8b39c6e1ef0c4f4e9f3
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-28 19:38:48 -0500

    FIX: saveFile method in DiskStorageProvider fails if uploads folder does not exist -Why? If the uploads folder did not exist at the time of the execution, our saveFile method would fail. How: This block solves the problem by checking if the uploads folder exists. If it does not exist yet: it creates the folder for us.

--- src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -7,6 +7,12 @@ import IStorageProvider from '@shared/container/providers/StorageProvider/models
 
 export default class DiskStorageProvider implements IStorageProvider {
   public async saveFile(fileName: string): Promise<string> {
+    try {
+      await fs.promises.access(uploadConfig.uploadsFolder);
+    } catch (err) {
+      await fs.promises.mkdir(uploadConfig.uploadsFolder);
+    }
+
     await fs.promises.rename(
       path.resolve(uploadConfig.tmpFolder, fileName),
       path.resolve(uploadConfig.uploadsFolder, fileName),
