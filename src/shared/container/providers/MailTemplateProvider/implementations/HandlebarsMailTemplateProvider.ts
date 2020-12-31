4b19e9d6795c0de3bab3834589277683f1aff11a
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-31 04:08:41 -0500

    FEAT: Email Templates with Template Engine (Part IV) -Created HandlebarsMailTemplateProvider that is the implementation responsible for parsing templates to generate HTML. -This implementation uses the handlebars library.

---
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -0,0 +1,15 @@
+import handlebars from 'handlebars';
+
+import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+
+export default class HandlebarsMailTemplateProvider
+  implements IMailTemplateProvider {
+  public async parse({
+    template,
+    variables,
+  }: IParseMailTemplateDTO): Promise<string> {
+    const parseTemplate = handlebars.compile(template);
+    return parseTemplate(variables);
+  }
+}
