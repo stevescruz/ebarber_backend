c9a3fcdf912ed784eba31eb9954f27bad5038d7c
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-01 18:21:09 -0500

    FEAT: Email Templates with Template Engine (Part XII) -Updated provider container index to use container.resolve to instantiate EtherealMailProvider instead of using the new keyword. Why? This is necessary because using the new keyword would make it fail, since EtherealMailProvider now has a constructor that we use to inject the correct dependencies. Since it requires an injection of dependencies, we will do that by using container.resolve on it to instantiate EtheralMailProvider by injecting the correct dependencies. -Updated provider container index by moving container.registerSingleton<IMailTemplateProvider> from below to above container.registerInstance<IMailProvider>. Why? Since MailTemplateProvider is required as a dependency of EtherealMailProvider, we need to register MailTemplateProvider in the provider container, before registering EtherealMailProvider. If we did the opposite it would fail when using container.resolve, because our provider container would not have registered IMailTemplateProvider yet, thus not being able to inject it as a dependency for EtherealMailProvider.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -14,12 +14,12 @@ container.registerSingleton<IStorageProvider>(
   DiskStorageProvider,
 );
 
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  new EtherealMailProvider(),
-);
-
 container.registerSingleton<IMailTemplateProvider>(
   'MailTemplateProvider',
   HandlebarsMailTemplateProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve(EtherealMailProvider),
 );
