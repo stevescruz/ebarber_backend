cfbab1fd81c6963ea50c86580245a7f38425e8fe
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-03 04:23:45 -0500

    FEAT: Parse Separate Handlebars Template File (Part IV) -Updated HandlebarsMailTemplateProvider to receive file as as a parameter instead of template in the parse method. -Also updated to read file by using the fs module and assign the result to the templateFileContent variable. -Also updated to pass templateFileContent to handlebars.compile method instead of passing template directly. Why? Now our parse method from IMailTemplateProvider will use a separate template file instead of directly compiling a string.

--- src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,4 +1,5 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
@@ -6,10 +7,14 @@ import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvi
 export default class HandlebarsMailTemplateProvider
   implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+
+    const parseTemplate = handlebars.compile(templateFileContent);
     return parseTemplate(variables);
   }
 }
