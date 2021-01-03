e025f81fd8298420884b3127d0dd91c3af4081ff
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-03 04:21:32 -0500

    FEAT: Parse Separate Handlebars Template File (Part III) -Updated FakeMailTemplateProvider to remove all parameters required by the parse method. -Also removed the non-required IParseMailProviderDTO import. -Also updated to return 'Mail content' -It is still a placeholder method

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,12 +1,8 @@
-import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({
-    template,
-    variables,
-  }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
