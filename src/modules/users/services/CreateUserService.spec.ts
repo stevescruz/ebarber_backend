01f1d342ae2d84eacc7f151eda7e7c93c8a58aba
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-04 00:59:57 -0500

    REF: CreateUserService.spec.ts to comply with the DRY Principle -Used the beforeEach method from the jest library to reduce code repetition.

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -5,15 +5,21 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 
 import AppError from '@shared/errors/AppError';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUserService: CreateUserService;
+
 describe('CreateUser', () => {
-  it('should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUserService = new CreateUserService(
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUserService = new CreateUserService(
       fakeUsersRepository,
       fakeHashProvider,
     );
+  });
 
+  it('should be able to create a new user', async () => {
     const user = await createUserService.execute({
       name: 'Uther the Lightbringer',
       email: 'uther@blizzard.com',
@@ -25,13 +31,6 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a user with a previously used email', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUserService = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
     await fakeUsersRepository.create({
       name: 'Uther the Lightbringer',
       email: 'uther@blizzard.com',
