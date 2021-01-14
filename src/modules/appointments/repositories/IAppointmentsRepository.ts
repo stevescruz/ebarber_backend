57ed1d737f696f5f506275c9c2501c18169f125a
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-14 17:37:22 -0500

    REF: IAppointmentsRepository to implement the findProviderAppointmentsByDay

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -2,11 +2,15 @@ import Appointment from '@modules/appointments/infra/typeorm/entities/Appointmen
 
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindProviderAppointmentsByMonthDTO from '@modules/appointments/dtos/IFindProviderAppointmentsByMonthDTO';
+import IFindProviderAppointmentsByDayDTO from '@modules/appointments/dtos/IFindProviderAppointmentsByDayDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findProviderAppointmentsByMonth(
     data: IFindProviderAppointmentsByMonthDTO,
+  ): Promise<Appointment[]>;
+  findProviderAppointmentsByDay(
+    data: IFindProviderAppointmentsByDayDTO,
   ): Promise<Appointment[]>;
 }
