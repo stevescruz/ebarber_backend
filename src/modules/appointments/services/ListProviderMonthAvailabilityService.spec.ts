b3906eaaa66ac4fc4c5d809ed77f2c6588080ec1
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-14 01:57:50 -0500

    REF: List Provider Month Availability Service Spec guarantees that the service is not able to list as available a day without any slots

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -16,6 +16,7 @@ describe('ListProviderMonthAvailability', () => {
   });
 
   it('should be able to list all days and their availability from a given month for a given service provider', async () => {
+    const year = 2021;
     const month = 2;
     const firstTimeSlotStartsAt = 8;
     const lastTimeSlotEndsAt = 18;
@@ -28,20 +29,20 @@ describe('ListProviderMonthAvailability', () => {
       timeSlots.map(timeSlot =>
         fakeAppointmentsRepository.create({
           provider_id: 'ce619769-f7b3-40f8-b51f-54cbdbadb95f',
-          date: new Date(2021, month - 1, 12, timeSlot, 0, 0),
+          date: new Date(year, month - 1, 12, timeSlot, 0, 0),
         }),
       ),
     );
 
     await fakeAppointmentsRepository.create({
       provider_id: 'ce619769-f7b3-40f8-b51f-54cbdbadb95f',
-      date: new Date(2021, month - 1, 13, 12, 0, 0),
+      date: new Date(year, month - 1, 13, 12, 0, 0),
     });
 
     const availability = await listProviderMonthAvailabilityService.execute({
       provider_id: 'ce619769-f7b3-40f8-b51f-54cbdbadb95f',
-      year: 2021,
-      month: 2,
+      year,
+      month,
     });
 
     expect(availability).toEqual(
@@ -50,6 +51,36 @@ describe('ListProviderMonthAvailability', () => {
         { day: 12, available: false },
         { day: 13, available: true },
       ]),
+    );
+  });
+
+  it('should not be able to list as available a day without any slots', async () => {
+    const year = 2030;
+    const month = 5;
+    const firstTimeSlotStartsAt = 8;
+    const lastTimeSlotEndsAt = 18;
+    const timeSlots = Array.from(
+      { length: lastTimeSlotEndsAt - firstTimeSlotStartsAt },
+      (_, index) => index + firstTimeSlotStartsAt,
+    );
+
+    await Promise.all(
+      timeSlots.map(timeSlot =>
+        fakeAppointmentsRepository.create({
+          provider_id: 'ce619769-f7b3-40f8-b51f-54cbdbadb95f',
+          date: new Date(year, month - 1, 15, timeSlot, 0, 0),
+        }),
+      ),
+    );
+
+    const availability = await listProviderMonthAvailabilityService.execute({
+      provider_id: 'ce619769-f7b3-40f8-b51f-54cbdbadb95f',
+      year,
+      month,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([{ day: 15, available: false }]),
     );
   });
 });
