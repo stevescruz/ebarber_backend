22b47bf122735a1fb3b74fbbf099fc90f1a0e2dd
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-14 20:38:31 -0500

    FEAT: Implemented List Provider Day Availability Service that returns a list with hour availability in a given day for a given provider

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,57 @@
+import { inject, injectable } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  year: number;
+  month: number;
+  day: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+export default class ListProviderDayAvailability {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findProviderAppointmentsByDay(
+      { provider_id, year, month, day },
+    );
+    const workShiftStartsAt = 8;
+    const numberOfTimeSlots = 10;
+
+    const hoursOfDay = Array.from(
+      { length: numberOfTimeSlots },
+      (_, index) => index + workShiftStartsAt,
+    );
+
+    const availability = hoursOfDay.map(hour => {
+      const hasAppointmentInHour = appointments.filter(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInHour,
+      };
+    });
+
+    console.log(availability);
+
+    return availability;
+  }
+}
