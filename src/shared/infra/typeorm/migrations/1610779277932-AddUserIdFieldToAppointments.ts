4c20202168fba2ef9f7a1b45e253a0c4bc4367e3
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-16 02:41:39 -0500

    FEAT: Add User Id Field to Appointments Table Migration

---
+++ src/shared/infra/typeorm/migrations/1610779277932-AddUserIdFieldToAppointments.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddUserIdFieldToAppointments1610779277932
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'user_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentUser',
+        referencedTableName: 'users',
+        referencedColumnNames: ['id'],
+        columnNames: ['user_id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentUser');
+    await queryRunner.dropColumn('appointments', 'user_id');
+  }
+}
