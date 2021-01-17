e8bf68d893cfc24ddce5bf0006f1492525c86755
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-16 19:53:06 -0500

    REF: Appointments Controller adjusted to include user_id

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -7,6 +7,7 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 export default class AppointmentsController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { provider_id, date } = req.body;
+    const user_id = req.user.id;
 
     const parsedDate = parseISO(date);
 
@@ -14,6 +15,7 @@ export default class AppointmentsController {
 
     const appointment = await createAppointment.execute({
       provider_id,
+      user_id,
       date: parsedDate,
     });
 
