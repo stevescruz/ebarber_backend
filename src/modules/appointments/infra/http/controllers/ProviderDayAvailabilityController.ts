6c82a50e9e11308630cf94ad77454d6691e8f073
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-18 16:33:38 -0500

    FEAT: Provider Month Availability Controller -Implemented index method responsible for executing the List Provider Month Availability Service.

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const { provider_id } = req.params;
+    const { year, month, day } = req.body;
+
+    const listProviderDayAvailabilityService = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailabilityService.execute({
+      provider_id,
+      year,
+      month,
+      day,
+    });
+
+    return res.json(availability);
+  }
+}
