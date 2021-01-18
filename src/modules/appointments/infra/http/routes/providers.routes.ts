ac24f3591d8279b3f73114908425c00f4c78075a
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-18 16:35:06 -0500

    REF: Providers Routes to mount Provider Month Availability Controller & Provider Day Availability Controller

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticated from 'modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from 'modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
