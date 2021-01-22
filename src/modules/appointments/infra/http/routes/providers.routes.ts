0f336797951333b7d940041677fdde539e092112
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-22 16:56:16 -0500

    REF: Providers Routes with Data Validation Middleware -Guarantees that the request's route params provider_id is string, uuid and required for both the get '/:provider_id/month-availability and get '/:provider_id/day-availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ensureAuthenticated from 'modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from 'modules/appointments/infra/http/controllers/ProvidersController';
@@ -15,10 +16,20 @@ providersRouter.use(ensureAuthenticated);
 providersRouter.get('/', providersController.index);
 providersRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerMonthAvailabilityController.index,
 );
 providersRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerDayAvailabilityController.index,
 );
 
