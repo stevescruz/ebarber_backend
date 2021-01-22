9d10063b1294b056f7c3d3fc87bf8179dff7eab7
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-22 17:01:10 -0500

    REF: Profile Routes with Data Validation Middleware -The put '/' request's body:   a) name should be a string and required.   b) email shoud be a string, email and required.   c) old_password should be a string   d) new_password should be required when old_password exists   e) new_password_confirmation should be required and match new_password, when new_password exists.

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 
@@ -10,6 +11,24 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      new_password: Joi.when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+      }),
+      new_password_confirmation: Joi.when('new_password', {
+        is: Joi.exist(),
+        then: Joi.valid(Joi.ref('new_password')).required(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
