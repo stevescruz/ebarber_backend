dddb44787845794a2eaf869b72e235003c978a83
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-22 16:58:39 -0500

    REF: Password Routes with Data Validation Middleware -The post '/forgot' request's body email is string, email, required. -The post '/reset' request's body password is string and required, while the password confirmation should be required and identical to the password. And finally the token should be a string, uuid and required.

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,3 +1,5 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import { Router } from 'express';
 
 import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController';
@@ -7,7 +9,21 @@ const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/reset', resetPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({ [Segments.BODY]: { email: Joi.string().email().required() } }),
+  forgotPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+      token: Joi.string().uuid().required(),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default passwordRouter;
