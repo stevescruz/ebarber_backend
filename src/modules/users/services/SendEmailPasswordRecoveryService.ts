d5bd233d9b1bdd0c409c3037a333cde924b5fbcc
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-22 17:57:55 -0500

    REF: Send Email Password Recovery Service to use the environment variable for the App URL

--- src/modules/users/services/SendEmailPasswordRecoveryService.ts
+++ src/modules/users/services/SendEmailPasswordRecoveryService.ts
@@ -51,7 +51,7 @@ class SendEmailPasswordRecoveryService {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `http://localhost:3000/reset_password?token=${token}`,
+          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
         },
       },
     });
