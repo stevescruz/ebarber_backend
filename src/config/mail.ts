582ed7ed58bce09755f3cde3217644a7fbf79270
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-30 02:10:25 -0500

    REF: Mail Config to include default for Emails -defaults.from.name is the default email senders's name  if another is not specified. -defaults.from.email is the default email sender's email address if another is not specified.

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,19 @@
 interface IMailConfig {
   driver: 'ses' | 'ethereal';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: process.env.MAIL_EMAIL_ADDRESS_FROM,
+      name: process.env.MAIL_NAME_FROM,
+    },
+  },
 } as IMailConfig;
