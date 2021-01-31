48b16f77b81d77ae77d188fe86b734d5b557c432
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-31 06:05:38 -0500

    REF: Providers IoC Container to import the already registered singletons or instances from each provider -Why? Because it we do not need to include the logic for injecting the correct provider in this file.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
-import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from '@shared/container/providers/MailProvider/implementations/SESMailProvider';
-
-import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import '@shared/container/providers/StorageProvider';
+import '@shared/container/providers/MailTemplateProvider';
+import '@shared/container/providers/MailProvider';
