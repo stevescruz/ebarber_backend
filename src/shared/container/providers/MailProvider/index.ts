04bc389fabd0352ebeee566f7531f4be7be803d1
stevescruz <steve.silverio.cruz@gmail.com>
2021-01-31 06:02:57 -0500

    FEAT: Create container index for Mail Provider that injects the correct Provider depending on the Environment

---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from '@shared/container/providers/MailProvider/implementations/SESMailProvider';
+import mailConfig from '@config/mail';
+
+const providers = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  providers[mailConfig.driver],
+);
