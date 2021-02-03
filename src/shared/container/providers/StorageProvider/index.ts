c6fbf3d20681a378970a8e49d3464abab21e9741
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-02 23:48:23 -0500

    REF: Storage Provider IoC Container Index to inject the correct dependency to Storage Provider singleton

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -3,12 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+import S3StorageProvider from '@shared/container/providers/StorageProvider/implementations/S3StorageProvider';
+import uploadConfig from '@config/upload';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );
