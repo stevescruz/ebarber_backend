bd0a2fda94381e50a8df6306e68e639467d6cf58
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-02 23:44:35 -0500

    REF: Upload Config to contain AWS SES configuration -Also moved the storage property to the newly created multer property.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,47 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 const uploadsFolder = path.resolve(tmpFolder, 'uploads');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder,
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename: (req, file, callback) => {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename: (req, file, callback) => {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
-    },
-  }),
-};
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: { bucket: process.env.STORAGE_SES_BUCKET },
+  },
+} as IUploadConfig;
