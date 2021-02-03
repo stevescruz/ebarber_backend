2d92f558d607a620796867f71b1ab0c258f0de75
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-03 01:45:57 -0500

    REF: User Entity to return AWS Path for avatar_url when STORAGE_DRIVER is s3

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -7,6 +7,8 @@ import {
 } from 'typeorm';
 import { Exclude, Expose } from 'class-transformer';
 
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
@@ -33,9 +35,17 @@ class User {
 
   @Expose({ name: 'avatar_url' })
   get avatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `${process.env.AWS_SES_URL}/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
